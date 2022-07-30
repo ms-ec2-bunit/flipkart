024e2e6d81aa2217a70ad02b835f9c574681de47
rajithachanvelly <rajithachanvelly@gmail.com>
2022-07-30 20:25:40 +0530

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 this is a deploy.sh file and
 I am editing this file as part of git branch command practice
-I am rajitha chanvelly working for so and so organization
+I am rajitha chanvelly working for so and so organizatio
+I hava a baby girl who's name is mishitha reddy
 
