62021cd6324756c9da827d43ccb44b61de87cb0a
rajithachanvelly <rajithachanvelly@gmail.com>
2022-08-04 16:32:10 +0530

    uat Update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 this is a deploy.sh file and
 I am editing this file as part of git branch command practice
-I am rajitha chanvelly working for so and so organization
-
+I am working for so and so organization
+Editing as part of cherry-pick 
