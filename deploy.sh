3cb17a09d46426ddeeb82b9ec640bc8cdf7d1f01
Rajitha <rajithachanvelly@gmail.com>
2022-08-05 21:17:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 this is a deploy.sh file and
 I am editing this file as part of git branch command practice
-I am rajitha chanvelly working for so and so organizatio
+I am  working for so and so organizatio
 I hava a baby girl who's name is mishitha reddy
-
+this is as part of fetch command test
